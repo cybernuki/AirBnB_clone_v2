636ecfff7ddee663a3485bef9a4eea81eb7e936b
Feli <srgatoiscool@gmail.com>
2020-03-31 22:42:27 +0000

    fix permissions for the sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- setup for mysql for dev
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
