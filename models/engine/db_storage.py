4c16e9945c64160bbcf7cb9413e77bca8b4843f4
Feli <srgatoiscool@gmail.com>
2020-04-02 19:24:44 +0000

    add db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,9 +10,66 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 
+database = getenv("HBNB_MYSQL_DB")
+user = getenv("HBNB_MYSQL_USER")
+host = getenv("HBNB_MYSQL_HOST")
+password = getenv("HBNB_MYSQL_PWD")
+hbnb_env = getenv("HBNB_ENV")
+
 class DBStorage:
     """class DBStorage"""
     __engine = None
     __session = None
 
     def __init__(self):
+        """
+        """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format
+                (user, password, host, database),
+                pool_pre_ping=True)
+
+        if hbnb_env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        """
+        _dict = {}
+        if cls is None:
+            _obj = []
+            classes = ['User', 'State', 'City', 'Place', 'Review']
+            for _class in classes:
+                result = self.__session.query(eval(_class))
+                for res in result:
+                    _obj.append(res)
+        else:
+            _obj = self.__session.query(cls).all()
+        for obj in _obj:
+            key = obj.__class__.__name__ + "." + str(obj.id)
+            _dict[key] = obj
+        return _dict
+
+    def new(self, obj):
+        """
+        """
+        if obj:
+            self.__session.add()
+
+    def save(self):
+        """
+        """
+        if obj:
+            self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        """
+        if obj:
+            self.__session.delete(obj)
+    
+    def reload(self):
+        """
+        """
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
