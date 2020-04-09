de27528978489fbd490f6e93fee0f5b5c7177087
cybernuki <1164@holbertonschool.com>
2020-04-09 01:56:46 +0000

    create a script that tar the files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# this fabric script generates a .tgz archive
+# from the contest of the web_static folder
+# using the do_pack
+from fabric.api import local
+from os import path
+from datetime import datetime
+
+
+def do_pack():
+        now = datetime.utcnow()
+        file_ = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
+                                                              now.month,
+                                                              now.day,
+                                                              now.hour,
+                                                              now.minute,
+                                                              now.second)
+
+        if not path.isdir("versions"):
+                if local("mkdir -p versions").failed:
+                        return None
+        if local('tar -cvzf {} web_static'.format(file_)).failed:
+                 return None
+        return file_
