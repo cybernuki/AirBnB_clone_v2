b845ab4ba105ed91a14c88b4da5a4700529b6463
cybernuki <1164@holbertonschool.com>
2020-04-10 01:19:48 +0000

    improve script to set up the web server and use sudo directive

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # This fabscript deploys the web static content
 from os import path
-from fabric.api import run, env, put, local
+from fabric.api import sudo, env, put, local
 from datetime import datetime
 
 # Production hosts
@@ -34,7 +34,7 @@ def do_deploy(archive_path):
     """
     # Assure that given parameter is a file
     if not path.isfile(archive_path):
-        return False
+            return False
 
     # Paths variables
     file_ = archive_path.split("/")[-1]
@@ -43,33 +43,45 @@ def do_deploy(archive_path):
     data = "/data/web_static/releases/{}/".format(name)
     current = "/data/web_static/current"
 
+    # Send web server configuration
+    if put("0-setup_web_static.sh", "/tmp/").failed:
+            return False
+    # Give execution permissions
+    if sudo("chmod u+x /tmp/0-setup_web_static.sh").failed:
+            return False
+    # Execute script
+    if sudo("/tmp/0-setup_web_static.sh").failed:
+            return False
+    # Removes the script
+    if sudo("rm /tmp/0-setup_web_static.sh").failed:
+            return False
     # Send compressed file
     if put(archive_path, tmp).failed:
-        return False
+            return False
     # Remove a previous release
-    if run("rm -rf {}".format(data)).failed:
-        return False
+    if sudo("rm -rf {}".format(data)).failed:
+            return False
     # Create a directorty to allocate the new release
-    if run("mkdir -p {}".format(data)).failed:
-        return False
+    if sudo("mkdir -p {}".format(data)).failed:
+            return False
     # Uncompress archive
-    if run("tar -xzf {} -C {}".format(tmp, data)).failed:
-        return False
+    if sudo("tar -xzf {} -C {}".format(tmp, data)).failed:
+            return False
     # Remove compressed archive
-    if run("rm {}".format(tmp)).failed:
-        return False
+    if sudo("rm {}".format(tmp)).failed:
+            return False
     # Move uncomprresed data of web static to the release folder
-    if run("mv {}web_static/* {}".format(data, data)).failed:
-        return False
+    if sudo("mv {}web_static/* {}".format(data, data)).failed:
+            return False
     # Remove uncompressed data from sistem
-    if run("rm -rf {}web_static".format(data)).failed:
-        return False
+    if sudo("rm -rf {}web_static".format(data)).failed:
+            return False
     # Remove previous link to web static content
-    if run("rm -rf {}".format(current)).failed:
-        return False
+    if sudo("rm -rf {}".format(current)).failed:
+            return False
     # Create a new link to the new release content
-    if run("ln -s {} {}".format(data, current)).failed:
-        return False
+    if sudo("ln -s {} {}".format(data, current)).failed:
+            return False
     # Finish the deploy!
     print("New version deployed!")
 
