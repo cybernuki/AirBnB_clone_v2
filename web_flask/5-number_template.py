d7054ddf79482f74d5296bd0a4898c2179f2e7e1
cybernuki <1164@holbertonschool.com>
2020-04-20 21:40:07 +0000

    add template file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
@@ -24,6 +27,10 @@ def python_is_fun(text='is cool'):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
