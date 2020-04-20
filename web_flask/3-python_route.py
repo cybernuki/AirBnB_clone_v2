dbba266f4bbc5b1ae1263cd18e56d3e5ad5e9485
cybernuki <1164@holbertonschool.com>
2020-04-20 21:29:17 +0000

    add flask routing

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text =  text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_fun(text='is cool'):
+    text = text.replace('_', ' ')
+    return 'python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
