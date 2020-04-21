140171e4110608b790e6ef9ac70fe661cacf67ec
cybernuki <1164@holbertonschool.com>
2020-04-21 22:43:56 +0000

    pep 8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,17 +2,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    text =  text.replace('_', ' ')
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,18 +2,22 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    text =  text.replace('_', ' ')
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
