26e98c177901cf5fcd7f8821abb2ce8c15eff6d4
cybernuki <1164@holbertonschool.com>
2020-05-28 20:44:04 +0000

    challenge 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/airbnb-onepage', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
